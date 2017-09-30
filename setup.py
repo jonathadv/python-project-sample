e1adacf366b1679f4b0d9a71c29289570176e6a4
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2017-09-29 21:00:50 -0300

    Adds project structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='project',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Project Description',
+    author='Author',
+    license='MIT',
+    classifiers=[
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)
